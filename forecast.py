e3d538972653374410f109a17264c6dab6f39168
Statistical analysis and modelling (SCMA-632) VCU <87145248+scma-632@users.noreply.github.com>
2025-07-26 14:52:39 +0530

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,17 +1,18 @@
 import yfinance as yf
 import pandas as pd
-from pmdarima import auto_arima
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from datetime import timedelta
 
 def forecast_stock(ticker):
     df = yf.download(ticker, period="6mo", interval="1d")
     if df.empty:
-        raise ValueError("No data found. Please check the symbol.")
+        raise ValueError("No data found for this symbol.")
         
     df = df[['Close']].dropna()
 
-    model = auto_arima(df['Close'], seasonal=False, suppress_warnings=True)
-    forecast = model.predict(n_periods=5)
+    model = ExponentialSmoothing(df['Close'], trend='add', seasonal=None)
+    model_fit = model.fit()
+    forecast = model_fit.forecast(5)
 
     last_date = df.index[-1]
     forecast_dates = pd.bdate_range(start=last_date + timedelta(days=1), periods=5)
